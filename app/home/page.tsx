2fcb5d662bb44197a882af1e9af6f379beba4796
Osvaldo 'Jim' Jimenez 🤠 <osjimene@microsoft.com>
2025-06-20 11:45:28 -0500

    feat: remove unused data fetching functions and related imports from the codebase

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,18 +1,7 @@
-import { Card } from '@/app/ui/home/cards';
-import RevenueChart from '@/app/ui/home/revenue-chart';
-import LatestInvoices from '@/app/ui/home/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data'; 
+
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices  = await fetchLatestInvoices();
-    const { 
-        numberOfCustomers, 
-        numberOfInvoices, 
-        totalPaidInvoices, 
-        totalPendingInvoices 
-    } = await fetchCardData();
+
 
     return (
     <main>
@@ -131,20 +120,6 @@ export default async function Page() {
           </li>
         </ul>
       </section>
-      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
-      </div> */}
     </main>
   );
 }
